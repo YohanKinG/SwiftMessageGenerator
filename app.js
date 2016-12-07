07976614bb7e3bb8a7173dc6f4ab3c76578312e4
Yohan Gunathilaka <ykavindu@yahoo.com>
2016-12-07 13:03:42 +0530

    swift update

--- app.js
+++ app.js
@@ -7,16 +7,18 @@
 var http = require('http');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
-//var msgObj = require('./MTTypes');
 
 var app = express();
+
+//app view engine is set to jade
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/api.js
+++ routes/api.js
@@ -55,36 +55,39 @@ var mtFieldSpecification = [
 
 function createMsg(msgObj){
   if(msgObj.isTrueBlock1){
-  var ContentBlock1 = `{1:${msgObj.block1}}`;
-  swiftMsg = swiftMsg.concat(ContentBlock1);
+    var ContentBlock1 = `{1:${msgObj.block1}}`;
+    swiftMsg = swiftMsg.concat(ContentBlock1);
   }
   if(msgObj.isTrueBlock2){
-  var ContentBlock2 = `{2:${msgObj.mtId}${msgObj.block2}}`;
-  swiftMsg = swiftMsg.concat(ContentBlock2);
+    var ContentBlock2 = `{2:${msgObj.mtId}${msgObj.block2}}`;
+    swiftMsg = swiftMsg.concat(ContentBlock2);
   }
   if(msgObj.isTrueBlock3){
-  var ContentBlock3 = "";
-  swiftMsg = swiftMsg.concat(ContentBlock3);
+    var ContentBlock3 = '';
+    swiftMsg = swiftMsg.concat(ContentBlock3);
   }
   if(msgObj.isTrueBlock4){
-  var ContentBlock4 = "";
-  swiftMsg = swiftMsg.concat(ContentBlock4);
+    var ContentBlock4 = '{4:';
+    for (var key in msgObj.specification){
+        ContentBlock4.concat('\n');
+        ContentBlock4.concat(key.id+':'+key.Detail);        
+    }
+    swiftMsg = swiftMsg.concat(ContentBlock4+'}');
   }
   if(msgObj.isTrueBlock5){
-  var ContentBlock5 = "";
-  swiftMsg = swiftMsg.concat(ContentBlock5);
+    var ContentBlock5 = ``;
+    swiftMsg = swiftMsg.concat(ContentBlock5);
   }
 }
 
 /* Get the JSON file */
 router.get('/msg/', function(req, res, next) {
-  console.log("init");
+  createMsg(msgObj);
   var stream = fs.createWriteStream("./swiftMsg.txt");
-  stream.once('open',function(fd){
+  stream.once('open',function(){
     stream.write(swiftMsg);
     stream.end();
   });
-  createMsg(msgObj);
   res.json({"STATUS": "200 OK"});
 });
 
